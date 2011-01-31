331c7534071b995b9cd7436b05a1b904a77d55eb
Marlon Gomes Lopes <marlonglopes@gmail.com>
2011-01-31 14:55:53 -0300

    teste

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,9 @@ module Gereditais
     config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    #config.encoding = "uft-8"
+#    config.encoding = "uft-8"
+    #config.encoding = "ISO-8859-1"
+    
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
