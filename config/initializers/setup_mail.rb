d1674e23a8b5d37e914eae9367902a0b504f1194
Marlon Gomes Lopes <marlonglopes@gmail.com>
2011-02-07 18:36:08 -0300

    gmail account

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
   :domain               => "gmail.com",  
-  :user_name            => "marlonglopes",  
-  :password             => "62806280",  
+  :user_name            => "ginfghc",  
+  :password             => "fricazoid6280",  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }  
