9c7b04f839527c9f9984de4221775d149d9bb6df
Marlon Gomes Lopes <marlonglopes@gmail.com>
2011-02-09 23:16:22 -0200

    teste

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
     @user.accessible = :all if current_user.admin?
    	
     if @user.update_attributes(params[:user])
-      flash[:success] = "perfil do usuário atualizados."
+      flash[:success] = "perfil do usuário atualizado."
 #  		UserMailer.user_update(@user).deliver
   		UserMailer.delay.user_update(@user) 
       redirect_to root_path
