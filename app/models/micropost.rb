2a671c7316b511f8e2617771bb2344332bf87a75
Marlon Gomes Lopes <marlonglopes@gmail.com>
2011-02-10 14:56:07 -0300

    validate length of post

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,6 +9,8 @@ class Micropost < ActiveRecord::Base
 	
 	validates_presence_of :post, :message => 'Não é possível postar mensagem em branco'
 
+	validates_length_of :post, :maximum => 1000, :message => 'Não é possível postar mensagem com mais de 1000 caracteres'
+
 	attr_accessible :edital_id,:user_id,:post
 
 end
